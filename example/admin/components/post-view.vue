8b8a043eab9b5ab51268cd1a300181fb8ae885b6
Ivan Kalinichenko <kalinichenko@constlab.ru>
2019-07-28 17:34:01 +0300

    wip: add form component

--- example/admin/components/post-view.vue
+++ example/admin/components/post-view.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    Post View
+    <admin-form
+      :schema="schema"
+      @submit="submit"
+      @reset="reset"
+    />
   </div>
 </template>
+
+<script>
+  import { AdminForm } from '../../..'
+  import PostFormSchema from './post.form.json'
+
+  export default {
+    name: 'PostView',
+    components: {
+      AdminForm,
+    },
+    data() {
+      return {
+        schema: PostFormSchema,
+      }
+    },
+    methods: {
+      submit() {
+        console.log('submit')
+      },
+      reset() {
+        console.log('reset')
+      },
+    },
+  }
+</script>
