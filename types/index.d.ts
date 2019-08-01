58bef64f0d5eb145f0600740e5553b5d4bb88cea
Ivan Kalinichenko <kalinichenko@constlab.ru>
2019-08-01 19:50:33 +0300

    wip: loading blocks

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,3 +11,16 @@ export interface MenuItem {
 }
 
 export type SchemaForm = Component
+
+export interface Block {
+  id?: string
+  component?: string
+  props?: any
+}
+
+export interface BlockMeta {
+  title?: string
+  description?: string
+  icon?: string // default is 'extension'
+  group?: string // default is 'general'
+}
