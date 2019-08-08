af9f2321bfe6b2443792a96afd6d44b67587a2d5
Ivan Kalinichenko <kalinichenko@constlab.ru>
2019-08-08 17:35:39 +0300

    wip(blocks): add and remove block

--- example/admin/components/post-blocks.vue
+++ example/admin/components/post-blocks.vue
@@ -2,6 +2,8 @@
   <div v-if="page.blocks">
     <blocks-view
       :blocks="page.blocks"
+      @add-block="addBlock"
+      @remove-block="removeBlock"
     />
   </div>
 </template>
@@ -21,6 +23,21 @@
     },
     async created() {
       this.page = await this.$axios.$get('/pages/index.json')
+    },
+    methods: {
+      addBlock({ id, component, props }) {
+        this.page.blocks.push({
+          id,
+          component,
+          props,
+        })
+      },
+      removeBlock({ id }) {
+        const index = this.page.blocks.findIndex((item) => item.id === id)
+        if (index !== undefined) {
+          this.page.blocks.splice(index, 1)
+        }
+      },
     },
   }
 </script>
