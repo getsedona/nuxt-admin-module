6e3ce1e0de590c3c8a5458108c95bb88926d6cf7
Ivan Kalinichenko <kalinichenko@constlab.ru>
2019-08-09 12:39:36 +0300

    wip: seo form

--- example/admin/components/post-seo.vue
+++ example/admin/components/post-seo.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    SEO
+    <admin-form
+      :schema="schema"
+      @submit="submit"
+    />
   </div>
 </template>
 
 <script>
+  import { AdminForm } from 'nuxt-admin-module'
+  import SeoFormSchema from './seo.form.json'
+
   export default {
     name: 'PostSeo',
+    components: {
+      AdminForm,
+    },
+    data() {
+      return {
+        schema: SeoFormSchema,
+      }
+    },
+    methods: {
+      submit({ form }) {
+        console.log(form)
+      },
+    },
   }
 </script>
